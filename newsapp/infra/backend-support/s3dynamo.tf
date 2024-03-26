ba080ed31b2813db4eb71024b18c6ecf52d452dd
bibhutimailx <bibhutimailx@gmail.com>
2024-03-26 23:51:44 +0530

    newsapp update

---
+++ newsapp/infra/backend-support/s3dynamo.tf
@@ -0,0 +1,31 @@
+# This file creates S3 bucket to hold terraform states
+# and DynamoDB table to keep the state locks.
+resource "aws_s3_bucket" "terraform_infra" {
+  bucket = "news4321-terraform-infra-na"
+  acl    = "private"
+  force_destroy = true
+
+  # To allow rolling back states
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name           = "news4321-terraform-locks"
+  # up to 25 per account is free
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 2
+  write_capacity = 2
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+     Name = "Terraform Lock Table"
+     createdBy = "infra-news4321/backend-support"
+  }
+}
